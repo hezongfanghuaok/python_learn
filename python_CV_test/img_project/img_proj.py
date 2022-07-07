127a85f1280a22a129a165b408bf95be72909e79
hezongfanghuaok <754376164@qq.com>
2022-07-07 09:20:05 +0800

    tt

--- python_CV_test/img_project/img_proj.py
+++ python_CV_test/img_project/img_proj.py
@@ -6,6 +6,7 @@ matrix1 = np.array([[1034.7388916 ,  684.09613037], [772.74993896,   689.4161377
 matrix2 = np.array([[612.867,-65.59], [862.01,-69.66], [1113.16,-72.96],[614.92,83.25], [ 864.25,79.43], [1114.93,75.06],[ 618.15,283.03], [867.80,277.88], [1117.65,273.89]])
 
 mat2dtest,inlins=cv2.estimateAffine2D(matrix1,matrix2)
+cv2.calibrateHandEye()
 # cv写入yaml文件
 cv_file1 = cv2.FileStorage("mat2dtest.yaml", cv2.FILE_STORAGE_WRITE)
 print("write matrix\n", mat2dtest)
